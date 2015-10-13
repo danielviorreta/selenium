dbc70ca2032267bd7c0f93ac46633babceddaf54
Daniel Viorreta <daniel.viorreta@gmail.com>
2015-10-13 15:44:31 +0200

    Update node.sh

--- packer/node.sh
+++ packer/node.sh
@@ -3,4 +3,4 @@ sudo yum -y install docker
 sudo service docker start
 sudo chkconfig docker on
 sudo docker pull selenium/node-firefox
-sudo  docker run -d --link selenium-hub:hub -e TZ=Europe/Madrid selenium/node-firefox::2.47.1
+sudo  docker run -d --link selenium-hub:hub -e TZ=Europe/Madrid selenium/node-firefox:2.47.1
