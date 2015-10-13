6818608dccde0a22f551043b9dab8031f62b86f5
Daniel Viorreta <daniel.viorreta@gmail.com>
2015-10-13 14:34:06 +0200

    Update node.sh

--- packer/node.sh
+++ packer/node.sh
@@ -3,16 +3,4 @@ sudo yum -y install docker
 sudo service docker start
 sudo chkconfig docker on
 sudo docker pull selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
-sudo  docker run -d -e HUB_PORT_4444_TCP_ADDR=selenium-priv.infojobs.net -e HUB_PORT_4444_TCP_PORT=4444 -e TZ=Europe/Madrid selenium/node-firefox
+sudo  docker run -d -e TZ=Europe/Madrid --link selenium-hub:hub selenium/node-firefox
